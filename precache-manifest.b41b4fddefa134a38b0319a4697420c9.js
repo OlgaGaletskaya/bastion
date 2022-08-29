ec612cb2bce311eb28f164ab6111c5dda59ce8b9
OlgaGaletskaya <106168070+OlgaGaletskaya@users.noreply.github.com>
2022-08-29 17:37:57 +0300

    Updates

---
+++ precache-manifest.b41b4fddefa134a38b0319a4697420c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4683440b4ea75dbd976",
+    "url": "/bastion/static/css/main.d99c481f.chunk.css"
+  },
+  {
+    "revision": "a4683440b4ea75dbd976",
+    "url": "/bastion/static/js/main.a4683440.chunk.js"
+  },
+  {
+    "revision": "f6ea00d01bb49cc68503",
+    "url": "/bastion/static/js/1.f6ea00d0.chunk.js"
+  },
+  {
+    "revision": "d61865bdc00e443c9e7a",
+    "url": "/bastion/static/js/runtime~main.d61865bd.js"
+  },
+  {
+    "revision": "874fcbfb8ce0ff1d29a35cab95f01445",
+    "url": "/bastion/index.html"
+  }
+];
